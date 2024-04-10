ebb9a3e63188b69dd21066ce2d4670ef95aac05c
HauPham2k5 <hauphpd10095@gmail.com>
2024-04-10 15:07:32 +0700

    ASMgd2

--- Assets/Scripts/ColorSliderController.cs
+++ Assets/Scripts/ColorSliderController.cs
@@ -1,22 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ColorSliderController : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Slider slider;
+
+    // Scene name for the Game Over scene
+    public string gameOverSceneName = "GameOverScene";
+
     private void Start()
     {
         slider.value = 10f;
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Trap"))
         {
             Debug.Log("Trap");
-            slider.value--;
+            DecreaseSliderValue(1);
         }
+    }
+
+    void Update()
+    {
+        // Check if slider value reaches 0
+        if (slider.value <= 0)
+        {
+            // Load the Game Over scene
+            SceneManager.LoadScene(gameOverSceneName);
+        }
+    }
+
+    // Decrease slider value by specified amount
+    private void DecreaseSliderValue(float amount)
+    {
+        slider.value = Mathf.Max(slider.value - amount, 0f);
     }
 }
